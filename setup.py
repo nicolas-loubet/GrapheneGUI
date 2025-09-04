67100348557f9249f8c6f36bf08db3b5a596ab92
Nicolás Loubet <loubetnicolasalfredo@gmail.com>
2025-09-04 11:45:49 -0300

    Cambio versiones para conformidad.

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup, find_packages
 
 setup(
     name="GrapheneGUI",
-    version="1.0",
-    description="Interfaz gráfica basada en Qt para crear y funcionalizar placas de grafeno y óxido de grafeno",
+    version="2.2",
+    description="Qt-based graphical interface for creating and functionalizing graphene and graphene oxide plates",
     long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
     author="Nicolás Alfredo Loubet",
